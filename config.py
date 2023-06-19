22c17983db0e76b8b7a2d2564871f5889da483f0
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-06-19 10:56:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+TOKEN = os.environ.get("TOKEN", "6252902190:AAHQIY3hZidIbuJ3FEaAphDqf4nQcElzKQA")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "NYNQTF-BLZFFS-LTTPOP-KTRXBH-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/spacecloud")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
+OWNER = os.environ.get("OWNER", "5683195223")
+BOT_NAME = os.environ.get("BOT_NAME", "PoRN_HuB_Bot")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "spacecloud")
